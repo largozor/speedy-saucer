cec69332dddbcaeb7b991d0c9e82ef140fe8adb1
Josh Allen <largozor@josha.au>
2025-10-20 09:17:13 +1100

    SS-2.17-added-basic-input
    Added keyboard input for basic movement

--- player.gd
+++ player.gd
@@ -3,9 +3,20 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
 
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	var force = 1000
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force,0))
+	elif Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force,0))
+	elif Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
+	elif Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
 	pass
